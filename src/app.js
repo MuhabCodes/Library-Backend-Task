60529cfc1526a9d7442baf76e2808141aeb7f60f
Muhab Hossam <muhab.hossam13@gmail.com>
2024-10-01 12:17:34 +0300

    Added mongoose along with the book & user models

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 // src/app.js
 const express = require('express');
 const config = require('./config');
+const mongoose = require('mongoose')
 
 const app = express();
-
 app.use(express.json());
+
+mongoose.connect(config.mongoURI, {});
 
 module.exports = app;
